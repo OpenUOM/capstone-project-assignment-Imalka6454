2ce6317a2f5d7992765a4c37edcf6165e3a12324
Imalka6454 <59368707+Imalka6454@users.noreply.github.com>
2022-07-09 16:27:57 +0530

    Up N DB

--- backend/database.js
+++ backend/database.js
@@ -111,7 +111,7 @@ const readStudentInfo = async (id) => {
     const sql = `SELECT * FROM student WHERE id = ?`
     return new Promise((resolve, reject) => {
         knex_db
-            .raw(sql)
+            .raw(sql, [id])
             .then((student) => {
                 resolve(student);
             })
@@ -121,11 +121,11 @@ const readStudentInfo = async (id) => {
     });
 }
 
-const addStudent = async (id, name, age, Hometown) => {
-    const sql = `INSERT INTO student(id,name,age,Hometown) values (?, ?, ?, ?)`
+const addStudent = async (id, name, age, hometown) => {
+    const sql = `INSERT INTO student(id,name,age,hometown) values (?, ?, ?, ?)`
     return new Promise((resolve, reject) => {
         knex_db
-            .raw(sql)
+            .raw(sql, [id, name, age, hometown])
             .then(() => {
                 resolve({status: "Successfully inserted Student"})
             })
@@ -135,11 +135,11 @@ const addStudent = async (id, name, age, Hometown) => {
     });
 }
 
-const updateStudent = async (name, age, Hometown, id) => {
-    const sql = `UPDATE student SET name=?, age=?, Hometown=?, WHERE id=?`
+const updateStudent = async (name, age, hometown, id) => {
+    const sql = `UPDATE student SET name=?, age=?, hometown=?, WHERE id=?`
     return new Promise((resolve, reject) => {
         knex_db
-            .raw(sql)
+            .raw(sql, [name, age, hometown, id])
             .then(() => {
                 resolve({status: "Successfully updated Student"})
             })
@@ -153,7 +153,7 @@ const deleteStudent = async (id) => {
     const sql = `DELETE FROM student  WHERE id = ?`
     return new Promise((resolve, reject) => {
         knex_db
-            .raw(sql)
+            .raw(sql, [id])
             .then(() => {
                 resolve({status: "Successfully deleted Student"})
             })
